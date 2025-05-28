dbfe15b602b6f03c004524575b81a9bb4a0897a3
Cédric Luthi <cedric.luthi@gmail.com>
2025-05-28 23:01:47 +0200

    Add test that for passing options to the SqlBuilder() extension on IDbConnection

---
+++ src/InterpolatedSql.Dapper.Tests/InterpolatedSqlBuilderOptionsTests.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+using InterpolatedSql.SqlBuilders;
+using System.Data.SqlClient;
+
+namespace InterpolatedSql.Dapper.Tests;
+
+public class InterpolatedSqlBuilderOptionsTests
+{
+    [Test]
+    public void SqlBuilderWithOptions()
+    {
+        var cn = new SqlConnection();
+        var qb = cn.SqlBuilder(new InterpolatedSqlBuilderOptions { DatabaseParameterSymbol = ":" }, $"{"A"} {1}");
+        var sql = qb.Build().Sql;
+        Assert.AreEqual(":p0 :p1", sql);
+    }
+}
